edbb25a1d22c0254a2a3b0eb0b5907d380738c30
lucasdelacale <corp.lucasmartins@gmail.com>
2025-03-12 10:39:19 -0300

    Arquivo commit

---
+++ isights/commit.py
@@ -0,0 +1,17 @@
+'''
+git branch -M main  
+    ->  Caso seja o primeiro push do repositório
+git status  
+    ->  Mostra o status do repositório (caso precise)
+git add .  
+    ->  Adiciona todas as alterações
+git commit -m "Atualização do código"
+    ->  Cria um commit com descrição
+git push origin main
+    ->  Envia para o GitHub
+cd /Users/seu-usuario/caminho/do/projeto    
+    ->  Caso o Vscode não esteja na pasta do projeto
+
+git pull origin main --rebase
+    ->  Caso de erro por mudanças externas
+'''
